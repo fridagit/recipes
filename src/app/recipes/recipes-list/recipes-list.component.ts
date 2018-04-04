225687bea58cee991d8a9fd219f0310131994d2a
fridagit <frick.frida@gmail.com>
2018-04-04 14:59:17 +0200

    change backgroundimage in recipelist

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../../models';
 
 import { RecipesService } from '../../services/recipes.service';
@@ -8,15 +8,21 @@ import { RecipesService } from '../../services/recipes.service';
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent implements OnInit, OnDestroy {
     recipes: Recipe[];
 
-    constructor(private recipesService: RecipesService) { }
+    constructor(private recipesService: RecipesService) {
+      document.body.style.backgroundImage = "url('../../../assets/images/food.jpg')";
 
-  ngOnInit() {
+    }
 
-      this.recipes = this.recipesService.getRecipes();
-      console.log(this.recipes);
-  }
+    ngOnInit() {
 
+        this.recipes = this.recipesService.getRecipes();
+        console.log(this.recipes);
+    }
+
+    ngOnDestroy() {
+      document.body.style.backgroundImage = "url('../../../assets/images/home.jpg')";
+    }
 }
