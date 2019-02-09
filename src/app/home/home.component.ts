c7d684dfe12c8adaa0df1c07b194a8bdb159e03e
Johan Frick <johan.frick@gmail.com>
2019-02-09 13:59:08 +0100

    Remove save button on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { DataStorageService } from '../services/data-storage.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor(private dataService: DataStorageService) { }
-
-  ngOnInit() {
-  }
-
-  saveRecipes () {
-    this.dataService.storeRecipes();
-  }
-}
+export class HomeComponent {}
